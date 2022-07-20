aa83ecef2c36c360dd80f21630af29341179a649
Erick-DdS <erick.hidetto@hotmail.com>
2022-07-20 16:30:09 -0300

    Primeiros passos JS

---
+++ Fundamentos/bloco-4-introdução-JavaScript/scriptsNotas.js
@@ -0,0 +1,31 @@
+//if - se
+//else - senão
+
+const nota = 59;
+
+
+
+if (nota >= 80) {
+    console.log("Parabéns, você foi aprovado!");
+}else if (nota >= 60 && nota < 80) {
+    console.log("Você está na nossa lista de espera");
+}else{
+    console.log("Você foi reprovado.");
+}
+
+
+
+
+
+
+
+
+
+
+// if (condicao) {
+//     //código
+// }else if (outracondicao) {
+//     //outra condição
+// }else{
+//     //condição
+// }
---
+++ Fundamentos/bloco-4-introdução-JavaScript/scriptsTipagemEAritimetica.js
@@ -0,0 +1,19 @@
+let movie = "Avengers"; //string literal
+let score = 10.89; //number literal
+let isValid = true; //boolean
+let name; //undefined
+let color = null; //Aguarda entrada de valor
+
+//Operações aritiméticas com +. -. *, /, ** (exponencial).
+
+let salary = 3500;
+
+// console.log(salary+salary);
+// console.log(salary-salary);
+// console.log(salary*salary);
+// console.log(salary/salary);
+// console.log(3**3);
+
+salary--;
+
+console.log(salary);
