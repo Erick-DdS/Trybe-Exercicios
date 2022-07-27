79f17a931196914e1e750c4fd7277fc3483439bd
Erick-DdS <erick.hidetto@hotmail.com>
2022-07-27 15:11:10 -0300

    Primeira parte do conteúdo de seletores

---
+++ Fundamentos/bloco-4-DOM-e-Objetos4.5/seletores1.js
@@ -0,0 +1,7 @@
+const paragraph = document.getElementById("paragraph");
+      paragraph.style.color = "red";
+      document.getElementById("page-title").innerText = "Bastardos Inglórios";
+      document.getElementById("second-paragraph").innerText = "Antonio Margareeeti";
+const subtitle =  document.getElementById("subtitle");
+subtitle.innerText = "Gourlaaami";
+subtitle.style.color = "blue";
