983a9e6f030651ed2e4a0374aae267fdcb2369e9
Erick-DdS <erick.hidetto@hotmail.com>
2022-07-27 15:37:58 -0300

    Seletores com pilotos

---
+++ Fundamentos/bloco-4-DOM-e-Objetos4.5/pilotos.js
@@ -0,0 +1,7 @@
+let pilotosDeF1 = document.getElementsByClassName("piloto-f1-atual");
+
+for (let i = 0; i < pilotosDeF1.length; i++){
+    pilotosDeF1[i].innerText = "É Bota do Dragão powrra";
+}
+
+document.getElementsByClassName("lenda")[0].innerText = "Não é nemediano"
